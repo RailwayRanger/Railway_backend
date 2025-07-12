b2824c07b36f5acf94c356663682a5e9161490a5
yeesuho <suho080214@gmail.com>
2025-07-12 15:52:32 +0900

    fix: CORS 허용

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ CORS 허용 추가!
+  app.enableCors({
+    origin: '*', // Flutter 테스트용. 운영 시 도메인 지정 권장
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
